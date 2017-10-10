b954907abb6c849987e4254e7a780cec1f85b659
ukroma1992 <ukroma1992@gmail.com>
2017-10-10 16:08:59 +0300

    Added stripe and transactions controller

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem "paperclip", "~> 5.0.0"
 gem "dotenv-rails", group: :development
 gem 'aws-sdk', '~> 2.5.0'
 gem 'friendly_id', '~> 5.1.0'
+gem 'stripe'
 
 
 group :development, :test do
---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,28 @@
+class TransactionsController < ApplicationController
+
+  def create
+    book = Book.find_by!(slug: params[:slug])
+    token = params[:stripeToken]
+
+    begin
+      charge = Stripe::Charge.create(
+        amount: book.price,
+        currency: "uah",
+        card: token,
+        description: current_user.email
+      )
+      @sale = book.sales.create!(
+        buyer_email: current_user.email
+      )
+      redirect_to pickup_url(guid: @sale.guid)
+    rescue Stripe::CardError => e
+      @error = e
+      redirect_to books_path(book), notice @error
+    end
+  end
+
+  def pickup
+    @sale = Sale.find_by!(guid: params[:guid])
+    @book = @sale.book
+  end
+end
